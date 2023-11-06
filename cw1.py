1343f1802712a006ea346d8a63ddb03ccbe53160
Bh00fie <Bh00fie@gmail.com>
2023-11-06 18:20:40 +0000

    created a jupiter file for the assignment, calculated 4.1, 4.2, 4.3

---
+++ cw1.py
@@ -0,0 +1,51 @@
+# Aircraft Propulsion Code
+# Data Given
+Vcruise = 0.7 #Mach N
+height = 35000 #ft
+# Fan values
+FanPr = 1.45
+Fann = 0.94
+# LPC
+LPCPr = 1.45
+LPCn = 0.92
+#HPC
+HPCPr = 27.9
+HPCn = 0.92
+#HPT 
+HPTn = 0.92
+#LPT 
+LPTn = 0.94
+# Tail Cone fan
+TailConePr = 1.25
+TailConen = 0.96
+# Combustion Stage
+T04 = 1540 # K
+loss = 0.05 # Pressure lost
+# Air Properties
+cpAir = 1005 #JKg-1K-1
+gammaAir = 1.4
+# Combusion Products
+cpComb = 1100 #JKg-1K-1
+gammaComb = 1.3
+# Number of Fan Generators 
+nfg = 2
+# Overall Bypass Ratio
+BypassRatio = 14.4
+# Fan Gen Bypass Ratio
+FanGenBypass = 6.4
+# R1
+R1 = 0.35 #m
+# R2
+R2 = 1 #m
+# Boundary layer thickness
+h = 1 #m
+
+#General Information
+# At 35000 feet
+Tatm = 218.8 #k
+Patm = 23.8 #Kpa
+rhoAir = 0.38 #kg/m3
+
+# WORK
+# 4.1
+
